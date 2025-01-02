d69f7eb264796a22b2f182928d772e107e754e36
Amit chauhan <74815212+Amitjiran@users.noreply.github.com>
2025-01-02 19:27:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ const App = () => {
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
@@ -39,10 +39,12 @@ const App = () => {
         ))}
       </div>
 
+
+
       <div className='row' id="children-wrapper">
         {
           ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
+            <Selection key={key} title={key} applyColor={applyColor} />
           ))
         }
       </div>
